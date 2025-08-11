857d3d85e9a6895b1d99428eb235d30d41765a8f
apu <apusutradhar77@gmail.com>
2025-08-11 16:13:13 +0600

    added: why choose us section

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -2,7 +2,7 @@ import BannerSection from '@/components/pages/home/banner/banner-section';
 import FeaturedSection from '@/components/pages/home/featured-section/featured-section';
 import HeroSection from '@/components/pages/home/hero/hero-section';
 import StatsSection from '@/components/pages/home/stats/stats-section';
-// import WhyChooseUs from '@/components/pages/home/why-choose-us/why-choose-us';
+import WhyChooseUs from '@/components/pages/home/why-choose-us/why-choose-us';
 import React from 'react';
 
 const HomePage = async () => {
@@ -13,7 +13,7 @@ const HomePage = async () => {
            <BannerSection />
            <FeaturedSection />
            <StatsSection />
-           {/* <WhyChooseUs /> */}
+           <WhyChooseUs /> 
         </div>
     );
 };
